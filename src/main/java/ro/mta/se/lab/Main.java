15a0bcfd60cf6673b261f7090f9081e27e6b2dbe
Alex <alexandru.ghita98@gmail.com>
2021-01-13 16:39:50 +0200

    Integration of CityModel inside the controller
    Issue: #9
    I designed the city model and implemented it in the controller.
    I replaced the ChoiceBox with a ChechBox.
    The CheckBox now displays all the cities.
    
    Signed-off-by: Alex <alexandru.ghita98@gmail.com>

--- src/main/java/ro/mta/se/lab/Main.java
+++ src/main/java/ro/mta/se/lab/Main.java
@@ -1,28 +1,49 @@
 package ro.mta.se.lab;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import ro.mta.se.lab.Controller.WeatherController;
+import ro.mta.se.lab.Model.CityModel;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main extends Application {
+
+    private ObservableList<CityModel> cityModels = FXCollections.observableArrayList();
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void initializeCities()
-    {
+    public void initializeCities() {
+        try {
+            ArrayList<HashMap<String, String>> cityData = LocationFileReader.readFile("locations_input.txt");
+            System.out.println(cityData);
+            for (HashMap<String, String> data : cityData) {
+                cityModels.add(new CityModel(data.get("id"), data.get("name"),
+                        Float.parseFloat(data.get("lat")), Float.parseFloat(data.get("lon")), data.get("code")));
+            }
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void start(Stage primaryStage) {
         FXMLLoader loader = new FXMLLoader();
         try {
+            initializeCities();
 
             loader.setLocation(this.getClass().getResource("/view/weather.fxml"));
+            loader.setController(new WeatherController(cityModels));
             primaryStage.setScene(new Scene(loader.load()));
             primaryStage.setTitle("WeatherBoy");
             primaryStage.getIcons().add(new Image("/images/icon.png"));
